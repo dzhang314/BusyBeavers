f7910e95133e72967ece618fdfd603ade5f9b7f1
David K. Zhang <dzhang314@gmail.com>
2023-07-20 13:10:05 -0700

    modify BusyBeaverList to accept a tag

--- BusyBeaverList.jl
+++ BusyBeaverList.jl
@@ -1,47 +1,19 @@
 push!(LOAD_PATH, @__DIR__)
 using BusyBeavers
 
-function main(::Val{N}) where {N}
+function main(::Val{N}, tag::Vector{Int}, n::Int) where {N}
     @assert N > 1
-    labeled_machines = [
-        (
-            [0],
-            TuringMachine(set_rule(
-                TransitionTable{N}(),
-                DEFAULT_SYMBOL,
-                DEFAULT_STATE,
-                TransitionRule(
-                    TapeSymbol(false),
-                    RIGHT,
-                    State(DEFAULT_STATE.value + one(DEFAULT_STATE.value))
-                )
-            )),
-            0
-        ),
-        (
-            [1],
-            TuringMachine(set_rule(
-                TransitionTable{N}(),
-                DEFAULT_SYMBOL,
-                DEFAULT_STATE,
-                TransitionRule(
-                    TapeSymbol(true),
-                    RIGHT,
-                    State(DEFAULT_STATE.value + one(DEFAULT_STATE.value))
-                )
-            )),
-            0
-        )
-    ]
-    for i = 0:typemax(Int)
+    labeled_machines = [(
+        tag,
+        TuringMachine{N}(TuringMachine{N}(tag).transition_table),
+        0
+    )]
+    for i = 0:n
         println(
-            "Writing ",
-            length(labeled_machines),
-            " Turing machines to file after ",
-            i,
-            " steps."
+            "Writing ", length(labeled_machines),
+            " Turing machines to file after $i steps."
         )
-        filename = "BB" * string(N) * "-" * string(i; base=10, pad=8) * ".txt"
+        filename = "BB$N-$(join(tag, '.'))-$(string(i; base=10, pad=8)).txt"
         open(filename, "w+") do io
             for (label, tm, num_steps) in labeled_machines
                 println(
@@ -56,7 +28,7 @@ function main(::Val{N}) where {N}
                 )
             end
         end
-        println("Computing step ", i, ".")
+        println("Computing step ", i + 1, ".")
         labeled_successors = Vector{Tuple{Vector{Int},TuringMachine{N},Int}}()
         for (label, tm, num_steps) in labeled_machines
             if has_halted(tm)
@@ -97,4 +69,8 @@ function main(::Val{N}) where {N}
     end
 end
 
-main(Val{4}())
+main(
+    Val{parse(Int, ARGS[1])}(),
+    [parse(Int, piece) for piece in split(ARGS[2], '.')],
+    parse(Int, ARGS[3])
+)
